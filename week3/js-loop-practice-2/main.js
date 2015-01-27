aa205ac8034265fa721bd772e09c40912a50df70
Michael Herman <hermanmu@gmail.com>
2015-01-27 04:56:38 -0700

    refactor loop practice

--- week3/js-loop-practice-2/main.js
+++ week3/js-loop-practice-2/main.js
@@ -25,8 +25,8 @@ function secondLoop(arr) {
     // butterfly
     // marmot
     console.log("\nrat cat butterfly marmot");
-    for(var i=0; i<animals.length-1; i++) {
-        console.log(animals[i]);
+    for(var i=0; i<arr.length-1; i++) {
+        console.log(arr[i]);
     }
 }
 
@@ -35,8 +35,8 @@ function thirdLoop(arr) {
     // butterfly
     // ocelot
     console.log("\nrat butterfly ocelot");
-    for(var i=0; i<animals.length; i+=2) {
-        console.log(animals[i]);
+    for(var i=0; i<arr.length; i+=2) {
+        console.log(arr[i]);
     }
 }
 
@@ -47,9 +47,9 @@ function fourthLoop(arr) {
     // cat
     // rat
     console.log("\nocelot marmot butterfly cat rat");
-    var animalsReversed = animals.reverse();
-    for(var i=0; i<animals.length; i++) {
-        console.log(animalsReversed[i]);
+    var arrReversed = arr.reverse();
+    for(var i=0; i<arr.length; i++) {
+        console.log(arrReversed[i]);
     }
 }
 
@@ -63,12 +63,12 @@ function fifthLoop(arr) {
     // marmot
     // ocelot
     console.log("\nrat cat cat butterfly butterfly marmot marmot ocelot");
-    for(var i = 0; i < animals.length; i++) {
-        if(i === 0 || i === animals.length - 1) {
-            console.log( animals[i] );
+    for(var i = 0; i < arr.length; i++) {
+        if(i === 0 || i === arr.length - 1) {
+            console.log( arr[i] );
         } else {
             for(var b = 0; b < 2; b++) {
-                console.log( animals[i] );
+                console.log( arr[i] );
             }
         }
     }
