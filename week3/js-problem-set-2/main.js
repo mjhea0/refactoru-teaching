82052f830cfd42b37cab2a543d3de07a201588ac
Michael Herman <hermanmu@gmail.com>
2015-01-28 08:18:08 -0700

    firstreverse

--- week3/js-problem-set-2/main.js
+++ week3/js-problem-set-2/main.js
@@ -6,6 +6,32 @@ $(function() {
 
     console.log(firstReverse("dog"));
 
+
+    function swapCase(str) {
+        var characterArray = str.split('');
+        var newArray = [];
+        characterArray.forEach(function(letter) {
+            if (letter === letter.toUpperCase()) {
+                newArray.push(letter.toLowerCase());
+            } else {
+                newArray.push(letter.toUpperCase());
+            }
+        });
+        return newArray.join('');
+    }
+
+    console.log(swapCase("dog"));
+    console.log(swapCase("DoG"));
+
+
+    function letterCount(str) {
+        return str.split(' ').sort(function(firstWord, secondWord) {
+            return firstWord.length - secondWord.length;
+        })[0];
+    }
+
+    console.log(letterCount("Today, is the greatest day ever!"));
+
 });
 
 
