b35feb6d39ff016f8a90e0bf9b70e83bb89ddefc
Michael Herman <hermanmu@gmail.com>
2015-02-17 22:47:45 -0700

    updates

--- week6/node1-colors/part3_webcolors/color.js
+++ week6/node1-colors/part3_webcolors/color.js
@@ -2,17 +2,19 @@ var request = require('request'),
     _ = require('underscore'),
     userInput = process.argv[2].toLowerCase();
 
-console.log(checkUserColor(userInput));
+
+
+checkUserColor(userInput);
 
 function checkUserColor(color){
-  var test = '';
   request('https://cdn.rawgit.com/metaraine/swatch/74580660c9229541622bbf1fd4198618d9f677e5/webcolors.json',
     function (error, response, colors) {
-    if (!error && response.statusCode == 200) {
-      var colorsJson = JSON.parse(colors);
-      var rgb = _.findWhere(colorsJson, {name: colors}).rgb; // doesn't work
-      test += _.values(rgb).join(' ');
-    }
+      if (!error && response.statusCode == 200) {
+        var colorsJson = JSON.parse(colors);
+        var getObject = _.find(colorsJson, function(obj) {
+          return obj.name.toLowerCase() === color;
+        });
+        console.log(getObject.rgb.r+" "+getObject.rgb.g+" "+getObject.rgb.b);
+      }
   });
-  return test;
 }
