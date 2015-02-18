26770892b75fed8d86563f273276d9ee91743899
Michael Herman <hermanmu@gmail.com>
2015-02-18 12:27:47 -0700

    updated problem set 4

--- week6/js-problem-set-4/main.js
+++ week6/js-problem-set-4/main.js
@@ -29,3 +29,25 @@ function addNumbers(str) {
 console.log(addNumbers("88Hello 3World!"));
 console.log(addNumbers("55Hello"));
 console.log(addNumbers("5Hello 5"));
+
+
+// *** longest word ** ///
+
+
+function longestWord(str) {
+  // create an array
+  var words = str.split(" ");
+  // grab first word
+  var theLongestWord = words.shift();
+  // compare with a for loop
+  for(var i = 0; i < words.length; i++) {
+    if(words[i].length > theLongestWord.length) {
+      theLongestWord = words[i];
+    }
+  }
+  return theLongestWord;
+}
+
+console.log(longestWord('one two three'));
+console.log(longestWord('.one three'));
+console.log(longestWord('one two'));
